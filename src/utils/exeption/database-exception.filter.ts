63b5f43dab53327be5203b95c52bde20ca0866bd
fababy <studev170@gmail.com>
2023-04-17 20:49:39 +0200

    remove useless exception filter
     I deleted filter for the exception

--- src/utils/exeption/database-exception.filter.ts
+++
@@ -1,8 +0,0 @@
-import { HttpException, HttpStatus } from "@nestjs/common"
-
-export class DatabaseConnectionException extends HttpException {
-
-    constructor() {
-        super('Unable to connect to the database', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
